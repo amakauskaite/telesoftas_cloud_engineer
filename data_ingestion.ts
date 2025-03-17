ea89632712a9722df74bd91d22c70b310b229904
Aušrinė M. <ausre.makauskaite@gmail.com>
2025-03-17 15:08:40 +0200

    Remove unused import and functions

--- data_ingestion.ts
+++ data_ingestion.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import * as AWS from 'aws-sdk';
 import * as Papa from 'papaparse';
-import * as stream from 'stream';
 
 const BUCKET_NAME = 'auma-spotify'; 
+
 const ARTISTS_URL = 'https://www.kaggle.com/datasets/yamaerenay/spotify-dataset-19212020-600k-tracks?select=artists.csv';
 const TRACKS_URL = 'https://www.kaggle.com/datasets/yamaerenay/spotify-dataset-19212020-600k-tracks?select=tracks.csv';
 const ARTISTS_FILENAME = 'artists.csv'
@@ -25,18 +25,6 @@ async function downloadCSV(url: string): Promise<any[]> {
     });
   });
 }
-
-// This function returns a stream of data instead of data already parsed into a javascript object
-// Useful for large files, but then should have parsing added as well maybe?
-// Fetch the CSV file from the URL
-/*
-async function fetchCSV(url: string): Promise<stream.Readable> {
-  const response = await axios.get(url, {
-    responseType: 'stream',
-  });
-  return response.data;
-}
-  */
 
 // Filter tracks file
 // Should only have rows where there's a track name AND the tracks is longer than 1 minute (or 1 minute long)
@@ -66,35 +54,3 @@ async function uploadCsvToS3(fileContent: Buffer, fileName: string, bucketName: 
   }
 }
 
-// Process the CSV file and upload to S3
-async function processAndUploadCSV(fileURL: string, fileName: string) {
-  try {
-    // Fetch the CSV file stream
-    const fileStream = await fetchCSV(fileURL);
-
-    // TODO: 
-    // 1. Filter tracks file to only have rows where name is not null (not empty) and duration_ms >= 60 000
-    // 2. Filter artists file to only have rows where artist_id is in the filtered tracks file
-    
-    // Optionally, you can parse the CSV here if needed
-   // const parsedData: any[] = [];
-    
-   // fileStream.pipe(csvParser())
-    //  .on('data', (row) => {
-        // Process each row of the CSV, if needed
-    //    parsedData.push(row);
-    //  })
-     // .on('end', async () => {
-        // After parsing, you can choose to upload the CSV directly
-        // or manipulate the data before uploading
-
-        // const fileName = 'your-csv-file.csv';  // Give the file a name when uploading to S3
-        await uploadToS3(fileStream, fileName);
-     // });
-  } catch (error) {
-    console.error('Error processing CSV file:', error);
-  }
-}
-
-processAndUploadCSV(ARTISTS_URL, ARTISTS_FILENAME);
-processAndUploadCSV(TRACKS_URL, TRACKS_FILENAME);
