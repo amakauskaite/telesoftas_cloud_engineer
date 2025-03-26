16612861a63c2e2758b79d2a4eba7238c92f639a
Aušrinė M. <ausre.makauskaite@gmail.com>
2025-03-26 14:40:44 +0200

    Add and test date explosion funct.

--- kaggle_ingestion/test_transformation.js
+++ kaggle_ingestion/test_transformation.js
@@ -1,9 +1,51 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var tc = require("./test_conversion");
+// Assign undefined if the month and/or day is missing
+function assignDateValues(dateParts, updatedJson) {
+    updatedJson['year'] = parseInt(dateParts[0], 10);
+    updatedJson['month'] = dateParts[1] ? parseInt(dateParts[1], 10) : undefined;
+    updatedJson['day'] = dateParts[2] ? parseInt(dateParts[2], 10) : undefined;
+}
+function explodeDateFieldsInJson(json, dateFieldName) {
+    return json.map(function (item) {
+        var updatedItem = __assign({}, item); // Create a shallow copy to avoid mutating the original object
+        var dateField = item[dateFieldName]; // Get the release date from the current object
+        // Check if the release date exists and is not undefined or null
+        if (dateField != null) {
+            var dateParts = void 0;
+            // If the release date is a string, split it by '-'
+            if (typeof dateField === 'string') {
+                dateParts = dateField.split('-');
+            }
+            // If the release date is a number (just the year), handle it accordingly
+            else if (typeof dateField === 'number') {
+                dateParts = [dateField.toString()]; // Treat it as just a year
+            }
+            else {
+                dateParts = []; // If the release date is neither string nor number, we can't process it
+            }
+            // Call the function to assign values to the updatedItem
+            assignDateValues(dateParts, updatedItem);
+        }
+        return updatedItem; // Return the modified item
+    });
+}
 var filePath = 'C:\\Users\\ausri\\OneDrive\\Documents\\GitHub\\telesoftas_cloud_engineer\\kaggle_ingestion\\some_tracks.csv';
 tc.parseCSVFileFromPath(filePath).then(function (data) {
     console.log('Parsed first item:', data[0]);
     console.log('Parsed item with not full date:', data[4]);
-    // console.log(data[0].artists[0]);
+    var updatedJson = explodeDateFieldsInJson(data, 'release_date');
+    console.log(updatedJson);
 });
--- kaggle_ingestion/test_transformation.ts
+++ kaggle_ingestion/test_transformation.ts
@@ -1,12 +1,54 @@
 import * as tc from './test_conversion';
 
+interface DynamicJson {
+    [key: string]: any; // Allows any key with any value
+}
+
+// Assign undefined if the month and/or day is missing
+function assignDateValues(dateParts: string[], updatedJson: DynamicJson) {
+    updatedJson['year'] = parseInt(dateParts[0], 10);
+    updatedJson['month'] = dateParts[1] ? parseInt(dateParts[1], 10) : undefined;
+    updatedJson['day'] = dateParts[2] ? parseInt(dateParts[2], 10) : undefined;
+}
+
+function explodeDateFieldsInJson(json: DynamicJson[], dateFieldName: string): DynamicJson[] {
+    return json.map((item) => {
+        const updatedItem: DynamicJson = { ...item }; // Create a shallow copy to avoid mutating the original object
+
+        const dateField = item[dateFieldName]; // Get the release date from the current object
+        // Check if the release date exists and is not undefined or null
+        if (dateField != null) {
+            let dateParts: string[];
+
+            // If the release date is a string, split it by '-'
+            if (typeof dateField === 'string') {
+                dateParts = dateField.split('-');
+            }
+            // If the release date is a number (just the year), handle it accordingly
+            else if (typeof dateField === 'number') {
+                dateParts = [dateField.toString()]; // Treat it as just a year
+            } else {
+                dateParts = []; // If the release date is neither string nor number, we can't process it
+            }
+
+            // Call the function to assign values to the updatedItem
+            assignDateValues(dateParts, updatedItem);
+        }
+
+        return updatedItem; // Return the modified item
+    });
+}
+
 const filePath = 'C:\\Users\\ausri\\OneDrive\\Documents\\GitHub\\telesoftas_cloud_engineer\\kaggle_ingestion\\some_tracks.csv';
 
 tc.parseCSVFileFromPath(filePath).then((data) => {
     console.log('Parsed first item:', data[0]);
     console.log('Parsed item with not full date:', data[4]);
-    // console.log(data[0].artists[0]);
-  })
+
+    const updatedJson = explodeDateFieldsInJson(data, 'release_date');
+    console.log(updatedJson)
+
+})
 
 
 
