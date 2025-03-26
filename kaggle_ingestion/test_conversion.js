ca6e66b7e861e4ecad33612d50fe7c52c25285c3
Aušrinė M. <ausre.makauskaite@gmail.com>
2025-03-26 20:33:55 +0200

    Use size instead of lenght
    Because array was switched to set, their row count properties are different

--- kaggle_ingestion/test_conversion.js
+++ kaggle_ingestion/test_conversion.js
@@ -52,17 +52,20 @@ function parseCSVFileFromPath(filePath) {
         });
     });
 }
-/*
 // Example usage
-const filePath = 'C:\\Users\\ausri\\OneDrive\\Documents\\GitHub\\telesoftas_cloud_engineer\\kaggle_ingestion\\complex_artists_examples.csv';
-
+var filePath = 'C:\\Users\\ausri\\OneDrive\\Documents\\GitHub\\telesoftas_cloud_engineer\\kaggle_ingestion\\some_tracks.csv';
+// complex_artists_examples.csv';
 parseCSVFileFromPath(filePath)
-  .then((data) => {
-    console.log('Parsed CSV data:', data);
-    console.log(data[0].artists[0]);
-  })
-  .catch((error) => {
+    .then(function (data) {
+    // console.log('Parsed CSV data:', data);
+    // console.log(data[0].artists[0]);
+    console.log("Row count of tracks: ", data.length);
+    var artistsWithTracks = new Set(data.flatMap(function (track) { return track.artists; }));
+    console.log("Row count of artists values: ", artistsWithTracks.values.length);
+    console.log("Row count of artists entries: ", artistsWithTracks.entries.length);
+    console.log("Row count of artists entries: ", artistsWithTracks.size);
+    console.log(artistsWithTracks);
+})
+    .catch(function (error) {
     console.error(error);
-  });
-
-  */
+});
--- kaggle_ingestion/test_conversion.ts
+++ kaggle_ingestion/test_conversion.ts
@@ -56,17 +56,25 @@ export function parseCSVFileFromPath(filePath: string): Promise<any[]> {
   });
 }
 
-/*
+
 // Example usage
-const filePath = 'C:\\Users\\ausri\\OneDrive\\Documents\\GitHub\\telesoftas_cloud_engineer\\kaggle_ingestion\\complex_artists_examples.csv';
+const filePath = 'C:\\Users\\ausri\\OneDrive\\Documents\\GitHub\\telesoftas_cloud_engineer\\kaggle_ingestion\\some_tracks.csv';
+// complex_artists_examples.csv';
 
 parseCSVFileFromPath(filePath)
   .then((data) => {
-    console.log('Parsed CSV data:', data);
-    console.log(data[0].artists[0]);
+    // console.log('Parsed CSV data:', data);
+    // console.log(data[0].artists[0]);
+
+    console.log("Row count of tracks: ", data.length)
+
+    const artistsWithTracks = new Set(data.flatMap(track => track.artists))
+    console.log("Row count of artists values: ", artistsWithTracks.values.length)
+    console.log("Row count of artists entries: ", artistsWithTracks.entries.length)
+    console.log("Row count of artists entries: ", artistsWithTracks.size)
+    console.log(artistsWithTracks)
   })
   .catch((error) => {
     console.error(error);
   });
 
-  */
