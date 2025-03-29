9aa51aad88f8c366a2ec516665e0ab08e2fc00e1
Aušrinė M. <ausre.makauskaite@gmail.com>
2025-03-29 21:44:51 +0200

    Use describe-it to describe several scenarios

--- data_transformation/__tests__/transformations.test.ts
+++ data_transformation/__tests__/transformations.test.ts
@@ -1,17 +1,28 @@
 import * as trans from '../transformations'
 
-test("filterTracks should return tracks that are longer than 1 minute and have a name", () =>
-{
-    const input = [
-        { id: 1, name: 'short song', duration_ms: 42 },
-        // nameless song
-        { id: 2, name: null, duration_ms: 80000 },
-        { id: 3, name: 'valid song', duration_ms: 90000 },
-      ];
-  
-      const expectedOutput = [
-        { id: 3, name: 'valid song', duration_ms: 90000 },
-      ];
+describe("filterTracks", () => {
+    it("should return tracks that are longer than 1 minute and have a name", () =>
+        {
+            const input = [
+                { id: 1, name: 'short song', duration_ms: 42 },
+                // nameless song
+                { id: 2, name: null, duration_ms: 80000 },
+                { id: 3, name: 'valid song', duration_ms: 90000 },
+              ];
+          
+              const expectedOutput = [
+                { id: 3, name: 'valid song', duration_ms: 90000 },
+              ];
+        
+            expect(trans.filterTracks(input)).toEqual(expectedOutput);
+        });
 
-    expect(trans.filterTracks(input)).toEqual(expectedOutput);
-})
+    it('should return an empty array if all tracks are invalid', () => {
+        const input = [
+          { name: null, duration_ms: 60000 },
+          { name: 'Song A', duration_ms: 59999 },
+        ];
+        
+        expect(trans.filterTracks(input)).toEqual([]);
+      });
+});
