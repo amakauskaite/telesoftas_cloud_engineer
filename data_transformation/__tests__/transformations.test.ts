f7ea2ba61adc873c0d5a6be434a29531c45a01b2
Aušrinė M. <ausre.makauskaite@gmail.com>
2025-03-29 21:54:25 +0200

    Add jest for filterArtists

--- data_transformation/__tests__/transformations.test.ts
+++ data_transformation/__tests__/transformations.test.ts
@@ -1,28 +1,58 @@
 import * as trans from '../transformations'
 
 describe("filterTracks", () => {
-    it("should return tracks that are longer than 1 minute and have a name", () =>
-        {
-            const input = [
-                { id: 1, name: 'short song', duration_ms: 42 },
-                // nameless song
-                { id: 2, name: null, duration_ms: 80000 },
-                { id: 3, name: 'valid song', duration_ms: 90000 },
-              ];
-          
-              const expectedOutput = [
-                { id: 3, name: 'valid song', duration_ms: 90000 },
-              ];
-        
-            expect(trans.filterTracks(input)).toEqual(expectedOutput);
-        });
+    it("should return tracks that are longer than 1 minute and have a name", () => {
+        const input = [
+            { id: 1, name: 'short song', duration_ms: 42 },
+            // nameless song
+            { id: 2, name: null, duration_ms: 80000 },
+            { id: 3, name: 'valid song', duration_ms: 90000 },
+        ];
+
+        const expectedOutput = [
+            { id: 3, name: 'valid song', duration_ms: 90000 },
+        ];
+
+        expect(trans.filterTracks(input)).toEqual(expectedOutput);
+    });
 
     it('should return an empty array if all tracks are invalid', () => {
         const input = [
-          { name: null, duration_ms: 60000 },
-          { name: 'Song A', duration_ms: 59999 },
+            { name: null, duration_ms: 60000 },
+            { name: 'Song A', duration_ms: 59999 },
         ];
-        
+
         expect(trans.filterTracks(input)).toEqual([]);
-      });
+    });
+});
+
+describe("filterArtists", () => {
+    it("should return only artists that have tracks in the filtered tracks list", () => {
+        const artists_input = [
+            { id: 1, name: 'Artist A' },
+            { id: 2, name: 'Artist "Nickname" B' },
+            { id: 3, name: 'Artist A\'s son' },
+        ];
+        const artistsFromTracks_input =
+            new Set(['Artist A', 'Artist A\'s son']);
+
+        const expectedOutput = [
+            { id: 1, name: 'Artist A' },
+            { id: 3, name: 'Artist A\'s son' },
+        ];
+
+        expect(trans.filterArtists(artists_input, artistsFromTracks_input)).toEqual(expectedOutput);
+    });
+
+    it('should return an empty array if no artists were in the artistsFromTracks list', () => {
+        const artists_input = [
+            { id: 1, name: 'Artist A' },
+            { id: 2, name: 'Artist "Nickname" B' },
+            { id: 3, name: 'Artist A\'s son' },
+        ];
+        const artistsFromTracks_input =
+            new Set([]);
+
+        expect(trans.filterArtists(artists_input, artistsFromTracks_input)).toEqual([]);
+    });
 });
