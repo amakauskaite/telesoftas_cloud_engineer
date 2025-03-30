c74906793c56c3579cc369b82af51906c0dd3405
Aušrinė M. <ausre.makauskaite@gmail.com>
2025-03-30 14:00:32 +0300

    Move artists to a CTE

--- analysis_views/followed_artist_tracks.sql
+++ analysis_views/followed_artist_tracks.sql
@@ -1,12 +1,20 @@
 CREATE OR REPLACE VIEW views.followed_artist_tracks
 AS
+-- Filtering to have less data scanned when joining
+WITH artists_with_followers  AS
+(
+	SELECT
+		id as artist_id,
+		name as artist_name
+	FROM music.artists
+	WHERE followers > 0
+)
 SELECT
-	a.id as artist_id,
-	a.name as artist_name,
+	artist_id,
+	artist_name,
 	t.id as track_id,
 	t.name as track_name
-FROM music.artists a
-JOIN music.tracks t ON a.id = ANY(t.id_artists)
-WHERE a.followers > 0
+FROM artists_with_followers a
+JOIN music.tracks t ON a.artist_id = ANY(t.id_artists)
 ORDER BY artist_id
 ;
